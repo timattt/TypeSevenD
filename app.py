cd8b46c9538c73231a354cc73e6279c2f607d837
Tim Trofimenko <timattt@gmail.com>
2024-01-11 19:27:35 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,12 +2,14 @@ from flask import Flask
 import json
 import os
 import psycopg2
+from flask import request
 
 app = Flask(__name__)
 
 @app.route('/task')
 def index():
-    print("Test endpoint!")
+    id = request.args.get('id')
+    app.logger.debug("Task with id: {}".format(id))
     conn = psycopg2.connect(host=os.environ['TYPE6_DATABASE_URL'], port=os.environ['TYPE6_DATABASE_PORT'], sslmode="verify-full", dbname="type-6-database", user=os.environ['TYPE6_DATABASE_USER'], password=os.environ['TYPE6_DATABASE_PASSWORD'])
     cursor = conn.cursor()
     cursor.execute('select * from type6user')
